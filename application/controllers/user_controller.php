315085c4d7a905928a2a9def38267471a270127f
unknown <prince262150@gmail.com>
2018-10-06 11:45:52 +0530

    E-commerce Website

---
+++ application/controllers/user_controller.php
@@ -0,0 +1,9 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class User_controller extends CI_Controller {
+
+	function index(){
+		echo 'welcome to user';
+	}
+}
---
+++ application/views/admin/brand/add_brand_form.php
@@ -0,0 +1,16 @@
+<?php include_once dirname(__FILE__).'/../header.php';?>
+
+<div class="row">
+
+	<div class="col-md-4 col-md-offset-2">
+				<h3>Add New Brand</h3>
+		<hr>
+		<form action="<?php echo base_url('admin_controller/insert_brand');?>" method="POST">
+			<label>Brand Name</label>
+			<input type="text" name="brand_name" class="form-control">
+			<?php echo form_error('brand_name');?>
+			<button class="btn btn-primary">Add Brand</button>
+		</form>
+	</div>
+</div>
+<?php include_once dirname(__FILE__).'/../footer.php';?>
